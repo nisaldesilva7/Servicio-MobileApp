ef61b952333d7adca2be509c37155d03e52315dc
nisaldesilva7 <nisaldesilva7@gmail.com>
2020-08-18 02:33:54 +0530

    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:servicio/models/user.dart';
-import 'package:servicio/screens/splash_loading/splash_loading.dart';
 import 'package:servicio/screens/wrapper.dart';
 import 'package:servicio/services/auth.dart';
 import 'package:servicio/router.dart' as router;
 import 'package:servicio/services/service_locator.dart';
+
+
 
 void main() {
   setupLocator();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context){
     return StreamProvider<User>.value(
